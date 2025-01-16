482a7c0457754a5c079d59adcc00bb2a8bb1ea2c
Matt <matiaslucero21884@gmail.com>
2025-01-16 19:54:53 -0300

    Commiteando Flix

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2025 Your Company Name
+ * All rights reserved.
+ */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
